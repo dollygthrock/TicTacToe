caeed345df9754b81df0b13ad4d439f9f6e546b9
dollygthrock <dollygthrock@gmail.com>
2016-04-09 15:30:01 -0400

    Behind the scenes TTT

---
+++ Test_Tic_Tac_Board.rb
@@ -0,0 +1,42 @@
+require "minitest/autorun"
+require_relative "Tic_Tac_Board.rb"
+
+class TestTicTacBoard < Minitest::Test
+
+def test_create_new_board
+	assert_equal([1,2,3,4,5,6,7,8,9],create_new_board)
+end 
+
+def test_update_board
+	assert_equal(["X",2,3,4,5,6,7,8,9],update_board([1,2,3,4,5,6,7,8,9],0,"X"))
+	assert_equal([1,2,3,4,5,6,7,8,"X"],update_board([1,2,3,4,5,6,7,8,9],8,"X"))
+end 
+
+def test_valid_position_on_game_board
+	assert_equal(true,valid_position_on_game_board([1,2,3,4,5,6,7,8,9],3))
+	assert_equal(true,valid_position_on_game_board([1,2,3,4,5,6,7,8,9],7))
+	assert_equal(true,valid_position_on_game_board([1,2,3,4,5,6,7,8,9],2))
+	assert_equal(false,valid_position_on_game_board([1,2,3,4,"O",6,7,8,9],4))
+	assert_equal(false,valid_position_on_game_board([1,2,3,4,5,6,7,8,9],14))
+end 
+
+def test_winner_determination
+	assert_equal(true,winner_determination(["X","X","X",4,5,6,7,8,9]))
+	assert_equal(false,winner_determination(["X",2,"X",4,5,6,7,8,"X"]))
+	assert_equal(false,winner_determination([1,2,3,"X",5,"X","X",8,9]))
+end 
+
+def test_all_winning_combinations
+	assert_equal(true,all_winning_combinations(["X","X","X",4,5,6,7,8,9],"X"))
+	assert_equal(true,all_winning_combinations([1,2,"X",4,5,"X",7,8,"X"],"X"))
+	assert_equal(true,all_winning_combinations([1,2,"X",4,"X",6,"X",8,9],"X"))
+	assert_equal(true,all_winning_combinations(["X",2,3,4,"X",6,7,8,"X"],"X"))
+	assert_equal(true,all_winning_combinations(["O",2,3,4,"O",6,7,8,"O"],"O"))
+end 
+
+# def test_game_ends_tie
+# 	assert_equal(true,game_ends_tie(["X","X","O","0","X","O","X","O","X"]))
+# end 
+
+
+end 
---
+++ Tic_Tac_Board.rb
@@ -0,0 +1,56 @@
+def create_new_board
+	board = [1,2,3,4,5,6,7,8,9]
+end 
+
+def update_board(game_board, position, marker)
+	game_board[position] = marker	
+	game_board
+end 
+
+def valid_position_on_game_board(game_board,position)
+	if game_board[position] == position + 1
+		true
+	else
+		false
+	end
+end 
+
+ def winner_determination(game_board)
+ 	 if game_board == ["X","X","X",4,5,6,7,8,9]
+ 	 	true
+ 	 else 
+ 	 	false
+ 	 end 
+ end 
+
+ def all_winning_combinations(game_board,marker)
+ 	if game_board[0] == marker && game_board[1] == marker && game_board[2] == marker ||
+ 	   game_board[3] == marker && game_board[4] == marker && game_board[5] == marker ||
+ 	   game_board[6] == marker && game_board[7] == marker && game_board[8] == marker ||
+ 	   game_board[0] == marker && game_board[3] == marker && game_board[6] == marker ||
+ 	   game_board[1] == marker && game_board[4] == marker && game_board[7] == marker ||
+ 	   game_board[2] == marker && game_board[5] == marker && game_board[8] == marker ||
+ 	   game_board[0] == marker && game_board[4] == marker && game_board[8] == marker ||
+ 	   game_board[2] == marker && game_board[4] == marker && game_board[6] == marker
+ 	   true
+ 	else
+ 	   false
+ 	end
+ end 
+
+ # def game_ends_tie(game_board,marker)
+ # 	 if all_winning_combinations(game_board,marker) == false
+ # 	 	game_board.each do |value|
+ # 	 		if value == "X" || value == "O"
+ # 	 			true #means there is a tie as an X or O in all indices
+ # 	 		else 
+ # 	 			false #means some indices do not have a marker
+ # 	 		end 
+
+ # 	 end 
+ # end 
+
+
+
+
+
