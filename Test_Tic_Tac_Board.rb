b07a05ceaa5f4548fba5b177e3ebf0e5c7794eee
dollygthrock <dollygthrock@gmail.com>
2016-04-10 10:43:23 -0400

    Ending in tie-all working

--- Test_Tic_Tac_Board.rb
+++ Test_Tic_Tac_Board.rb
@@ -20,22 +20,19 @@ def test_valid_position_on_game_board
 	assert_equal(false,valid_position_on_game_board([1,2,3,4,5,6,7,8,9],14))
 end 
 
-def test_winner_determination
-	assert_equal(true,winner_determination(["X","X","X",4,5,6,7,8,9]))
-	assert_equal(false,winner_determination(["X",2,"X",4,5,6,7,8,"X"]))
-	assert_equal(false,winner_determination([1,2,3,"X",5,"X","X",8,9]))
-end 
-
 def test_all_winning_combinations
 	assert_equal(true,all_winning_combinations(["X","X","X",4,5,6,7,8,9],"X"))
 	assert_equal(true,all_winning_combinations([1,2,"X",4,5,"X",7,8,"X"],"X"))
 	assert_equal(true,all_winning_combinations([1,2,"X",4,"X",6,"X",8,9],"X"))
 	assert_equal(true,all_winning_combinations(["X",2,3,4,"X",6,7,8,"X"],"X"))
 	assert_equal(true,all_winning_combinations(["O",2,3,4,"O",6,7,8,"O"],"O"))
+	assert_equal(false,all_winning_combinations(["X",2,"X",4,5,6,7,8,"X"],"X"))
+	assert_equal(false,all_winning_combinations([1,2,3,"X",5,"X","X",8,9],"X"))
 end 
 
 def test_game_ends_tie
 	assert_equal(true,game_ends_tie(["X","X","O","X","O","O","X","O","X"]))
+	assert_equal(false,game_ends_tie(["X","X",3,4,5,6,"X",8,"X"]))
 end
 
 end 
--- Tic_Tac_Board.rb
+++ Tic_Tac_Board.rb
@@ -15,13 +15,6 @@ def valid_position_on_game_board(game_board,position)
 	end
 end 
 
- def winner_determination(game_board)
- 	 if game_board == ["X","X","X",4,5,6,7,8,9]
- 	 	true
- 	 else 
- 	 	false
- 	 end 
- end 
 
  def all_winning_combinations(game_board,marker)
  	if game_board[0] == marker && game_board[1] == marker && game_board[2] == marker ||
@@ -39,12 +32,17 @@ end
  end 
 
  def game_ends_tie(game_board)
+ 		board_array = []
  	 	game_board.each do |value|
  	 		if value == "X" || value == "O" 
- 	 			board_array << value 
- 	 	        board_array.count = 9
- 	 		end 
+ 	 			board_array << value
+			end 
  	 	end
+ 	 if board_array.count == 9
+ 	 	true
+ 	 else
+ 	 	false
+ 	 end
  end
 
 
