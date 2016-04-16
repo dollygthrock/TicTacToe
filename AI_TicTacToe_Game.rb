f45180033bc1cf6df6dbe4df8408be34d0da48da
dollygthrock <dollygthrock@gmail.com>
2016-04-16 14:21:45 -0400

    changing player1 to current etc

--- AI_TicTacToe_Game.rb
+++ AI_TicTacToe_Game.rb
@@ -1,13 +1,13 @@
 require_relative "simple_AI.ttt_game.rb"
 require_relative "sequential.AI.game.rb"
 
-def play_game(player_1,player_2,game_board)
-	move = player_1[:player_mode].get_move(game_board)
-	game_board = update_board(game_board,move-1,player_1[:marker])
-		if game_over?(game_board,player_1[:marker]) 
+def play_game(current,opponent,game_board)
+	move = current[:player_mode].get_move(game_board)
+	game_board = update_board(game_board,move-1,current[:marker])
+		if game_over?(game_board,current[:marker]) 
 			game_board
 		else
-			play_game(player_2,player_1,game_board)
+			play_game(opponent,current,game_board)
 		end 
 end
 
